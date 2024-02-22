549c7b7c3a63f2ff3f39138e6a57d1e9a236315c
Vishwam Aggarwal <vishwam.aggarwal@gmail.com>
2024-02-21 19:04:59 -0800

    Basic Constructor and begin() implementation

--- vish_i2cdev/vish_i2cdev.cpp
+++ vish_i2cdev/vish_i2cdev.cpp
@@ -1,5 +1,34 @@
 #include "vish_i2cdev.h"
 
-int return25() {
-    return 25;
+vish_i2cdev::vish_i2cdev(uint8_t addr, bool i2cHW, uint32_t speed, uint8_t SDAPin, uint8_t SCLPin, bool PULLUP_Enable)
+{
+    _addr = addr;
+    _i2cHW = i2cHW;
+    _speed = speed;
+    _SDAPin = SDAPin;
+    _SCLPin = SCLPin;
+    _PULLUP_Enable = PULLUP_Enable;
+}
+
+bool vish_i2cdev::begin()
+{
+    if (!_i2cHW)
+    {
+        i2c_init(i2c0, _speed);
+    }
+    else
+    {
+        i2c_init(i2c1, _speed);       
+    }
+
+    gpio_set_function(_SDAPin, GPIO_FUNC_I2C);
+    gpio_set_function(_SCLPin, GPIO_FUNC_I2C);
+
+    if(_PULLUP_Enable)
+    {
+        gpio_pull_up(_SDAPin);
+        gpio_pull_up(_SCLPin);    
+    }
+
+    return true;
 }
--- vish_i2cdev/vish_i2cdev.h
+++ vish_i2cdev/vish_i2cdev.h
@@ -1,16 +1,22 @@
 #ifndef _VISH_I2CDEV_H
 #define _VISH_I2CDEV_H
 
-#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-
-int return25();
 
 class vish_i2cdev
 {
     public:
-    vish_i2cdev(uint8_t addr);
+    vish_i2cdev(uint8_t addr, bool i2cHW = 0, uint32_t speed = 400*1000
+        , uint8_t SDAPin = 1, uint8_t SCLPin = 2, bool PULLUP_Enable = false);
+    bool begin();
+
+    private:
+    uint8_t _addr;
+    bool _i2cHW;
+    uint32_t _speed;
+    uint8_t _SDAPin, _SCLPin;
+    bool _PULLUP_Enable;
 
 };
 
