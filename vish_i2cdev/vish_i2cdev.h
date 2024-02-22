7c7a2955e8f15cc559c5278367738828b519c500
Vishwam Aggarwal <vishwam.aggarwal@gmail.com>
2024-02-21 17:47:00 -0800

    Changing to cpp to use classes

--- vish_i2cdev/vish_i2cdev.h
+++ vish_i2cdev/vish_i2cdev.h
@@ -1,9 +1,17 @@
-#include "pico/stdlib.h"
-#include "hardware/i2c.h"
-
 #ifndef _VISH_I2CDEV_H
 #define _VISH_I2CDEV_H
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
 int return25();
+
+class vish_i2cdev
+{
+    public:
+    vish_i2cdev(uint8_t addr);
+
+};
 
 #endif // _VISH_I2CDEV_H
