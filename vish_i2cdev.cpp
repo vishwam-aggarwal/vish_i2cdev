09d14be2bc3e4c3e01336be331c1d929b990acab
Vishwam Aggarwal <vishwam.aggarwal@gmail.com>
2024-02-23 07:02:06 -0800

    Changed all possible functions parameters to use poiinters for params

--- vish_i2cdev.cpp
+++ vish_i2cdev.cpp
@@ -4,24 +4,20 @@ vish_i2cdev::vish_i2cdev()
 {
 }
 
-bool vish_i2cdev::begin(uint8_t addr, i2c_inst_t* i2c_port, uint32_t speed, uint8_t SDAPin, uint8_t SCLPin, bool PULLUP_Enable)
+bool vish_i2cdev::begin(uint8_t* addr, i2c_inst_t* i2c_port, uint* speed, uint8_t* SDAPin, uint8_t* SCLPin, bool* PULLUP_Enable)
 {
-    _addr = addr;
     _i2c_port = i2c_port;
-    _speed = speed;
-    _SDAPin = SDAPin;
-    _SCLPin = SCLPin;
-    _PULLUP_Enable = PULLUP_Enable;
+    _addr = addr;
 
-    i2c_init(_i2c_port, _speed);
+    i2c_init(i2c_port, *speed);
 
-    gpio_set_function(_SDAPin, GPIO_FUNC_I2C);
-    gpio_set_function(_SCLPin, GPIO_FUNC_I2C);
+    gpio_set_function(*SDAPin, GPIO_FUNC_I2C);
+    gpio_set_function(*SCLPin, GPIO_FUNC_I2C);
 
-    if(_PULLUP_Enable)
+    if(*PULLUP_Enable)
     {
-        gpio_pull_up(_SDAPin);
-        gpio_pull_up(_SCLPin);    
+        gpio_pull_up(*SDAPin);
+        gpio_pull_up(*SCLPin);    
     }
 
     return true;
@@ -29,8 +25,8 @@ bool vish_i2cdev::begin(uint8_t addr, i2c_inst_t* i2c_port, uint32_t speed, uint
 
 void vish_i2cdev::read(const uint8_t* reg, uint8_t* dat, size_t len)
 {
-    i2c_write_blocking(_i2c_port, _addr, reg, 1, true);
-    i2c_read_blocking(_i2c_port, _addr, dat, len, false);
+    i2c_write_blocking(_i2c_port, *_addr, reg, 1, true);
+    i2c_read_blocking(_i2c_port, *_addr, dat, len, false);
 
 }
 
@@ -42,5 +38,5 @@ void vish_i2cdev::write(const uint8_t* reg, uint8_t* dat, size_t len)
     {
         Data[i+1] = *(dat + i);
     }
-    i2c_write_blocking(_i2c_port, _addr, &Data[0], len+1, false); 
+    i2c_write_blocking(_i2c_port, *_addr, &Data[0], len+1, false); 
 }
--- vish_i2cdev.h
+++ vish_i2cdev.h
@@ -9,19 +9,15 @@ class vish_i2cdev
     public:
     vish_i2cdev();
     
-    bool begin(uint8_t addr, i2c_inst_t* i2c_port = i2c0, uint32_t speed = 400*1000
-        , uint8_t SDAPin = 1, uint8_t SCLPin = 2, bool PULLUP_Enable = false);
+    bool begin(uint8_t* addr, i2c_inst_t* i2c_port, uint* speed,
+                uint8_t* SDAPin, uint8_t* SCLPin, bool* PULLUP_Enable);
 
     void read(const uint8_t* reg, uint8_t* dat, size_t len);
     void write(const uint8_t* reg, uint8_t* dat, size_t len);
 
     private:
-    uint8_t _addr;
     i2c_inst_t* _i2c_port;
-    uint32_t _speed;
-    uint8_t _SDAPin, _SCLPin;
-    bool _PULLUP_Enable;
-
+    uint8_t* _addr;
 };
 
 #endif // _VISH_I2CDEV_H
